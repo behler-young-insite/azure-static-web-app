0d1b8cafbecf8ba831aa7193684794d703e50d65
Derek DenHartigh <derekd@behler-young.com>
2021-05-13 15:02:48 -0400

    first!

---
+++ scripts.js
@@ -0,0 +1,22 @@
+console.log("hello world!");
+
+//var helloWorldText = document.getElementsByClassName("hello-world")[0];
+var body = document.getElementsByTagName("body")[0];
+
+document.addEventListener("click", function (event) {
+	console.log(event);
+    if (!event.target.matches(".hello-world")) return;
+    event.preventDefault();
+	body.style.backgroundColor = getRandomColor();
+	body.style.color = getRandomColor();
+  },
+);
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
